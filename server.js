dd19243d515204d7d5e994cda39eaf65e57f67ba
Russ Wells <russ@russdigital.com>
2016-05-07 21:39:10 -0700

    using bcrypt

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ var express = require('express')
 var jwt = require('jwt-simple')
 var _ = require('lodash')
 var app = express()
+var bcrypt = require('bcrypt')
 app.use(require('body-parser').json())
 
-var users = [{username: 'dickeyxxx', password: 'pass'}]
+var users = [{username: 'dickeyxxx', password: '$2a$10$SqZw8TtcfcCJw1SxUf7Ub.yy6b.zzxvl347H6RjZiNXzOVBR3mrtO'}]
 var secretKey = 'supersecretkey'
 
 function findUserByUsername(username) {
 	return _.find(users, {username: username})
 }
 
-function validateUser(user, password) {
-	return user.password === password
+function validateUser(user, password, cb) {
+		bcrypt.compare(password, user.password, cb)
 }
 
 app.post('/session', function (req, res) {
 	var user = findUserByUsername(req.body.username)
-	if (!validateUser(user, req.body.password)) {
-		return res.send(401) // Unauthorized
-	}
-	var token = jwt.encode({username: user.username}, secretKey)
-	res.json(token)
+	validateUser(user, req.body.password, function (err, valid) {
+		if (err || !valid) { return res.send(401) }
+		var token = jwt.encode({username: user.username}, secretKey)
+		res.json(token)
+	})
 })
 
 app.get('/user', function (req, res) {

08c63795088c65d8c3c5ef63e2a5bd7fddcc62aa
Russ Wells <russ@russdigital.com>
2016-05-07 23:02:42 -0700

    Authentication with MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var jwt = require('jwt-simple')
-var _ = require('lodash')
+//var _ = require('lodash')
 var app = express()
 var bcrypt = require('bcrypt')
+var User = require('./user')
 app.use(require('body-parser').json())
 
-var users = [{username: 'dickeyxxx', password: '$2a$10$SqZw8TtcfcCJw1SxUf7Ub.yy6b.zzxvl347H6RjZiNXzOVBR3mrtO'}]
+//var users = [{username: 'dickeyxxx', password: '$2a$10$SqZw8TtcfcCJw1SxUf7Ub.yy6b.zzxvl347H6RjZiNXzOVBR3mrtO'}]
 var secretKey = 'supersecretkey'
-
+/*
 function findUserByUsername(username) {
 	return _.find(users, {username: username})
 }
@@ -15,21 +16,39 @@ function findUserByUsername(username) {
 function validateUser(user, password, cb) {
 		bcrypt.compare(password, user.password, cb)
 }
-
-app.post('/session', function (req, res) {
-	var user = findUserByUsername(req.body.username)
-	validateUser(user, req.body.password, function (err, valid) {
-		if (err || !valid) { return res.send(401) }
-		var token = jwt.encode({username: user.username}, secretKey)
-		res.json(token)
+*/
+app.post('/session', function (req, res, next) {
+	User.findOne({username: req.body.username})
+	.select('password')
+	.exec(function (err, user) {
+		if (err) { return next(err) }
+		if (!user) { return res.send(401) }
+		bcrypt.compare(req.body.password, user.password, function (err, valid) {
+			if (err) { return next(err) }
+			if (!valid) { return res.send(401)}
+			var token = jwt.encode({username: user.username}, secretKey)
+			res.json(token)
+		})
 	})
 })
 
 app.get('/user', function (req, res) {
 	var token = req.headers['x-auth']
-	var user = jwt.decode(token, secretKey)
-	//TODO: pull user info from database
-	res.json(user)
+	var auth = jwt.decode(token, secretKey)
+	User.findOne({username: auth.username}, function (err, user) {
+		res.json(user)	
+	})
+})
+
+app.post('/user', function (req, res, next) {
+	var user = new User({username: req.body.username})
+	bcrypt.hash(req.body.password, 10, function (err, hash) {
+		user.password = hash
+		user.save(function (err, user) {
+			if (err) { throw next(err) }
+			res.send(201)	
+		})
+	})
 })
 
 app.listen(3000)
